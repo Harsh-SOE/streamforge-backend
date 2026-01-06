182fc5c042ae9df5419724ad21731a8b41d4598d
Harsh-SOE <99632080+Harsh-SOE@users.noreply.github.com>
2026-01-06 11:58:49 +0530

    Feat/project config (#29)
    * feat: added editor config file
    
    * fix: adds colorful logs and secures scirpt to prevent command injection
    
    * fix: add playlist contract
    
    * fix: add playlist contract and environment check

--- apps/playlist/src/infrastructure/config/config.service.ts
+++ apps/playlist/src/infrastructure/config/config.service.ts
@@ -6,11 +6,16 @@ import * as protoLoader from '@grpc/proto-loader';
 import { GrpcOptions, Transport } from '@nestjs/microservices';
 import { HealthImplementation, protoPath as HealthCheckProto } from 'grpc-health-check';
 
+import { ENVIRONMENT } from '@app/utils/enums';
 import { PLAYLIST_PACKAGE_NAME } from '@app/contracts/playlist';
 
 @Injectable()
 export class PlaylistConfigService {
   public constructor(private readonly configService: ConfigService) {}
+
+  get NODE_ENVIRONMENT() {
+    return this.configService.getOrThrow<ENVIRONMENT>('NODE_ENVIRONMENT');
+  }
 
   get HTTP_PORT() {
     return this.configService.getOrThrow<number>('HTTP_PORT');
